1c7502b3439c374091fc0f224674bd4c63f8b370
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2024-02-18 20:32:46 +0700

    Add go to detail functionality

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -1,4 +1,5 @@
 abstract class AppPages {
-  static const String home = '/home';
   static const String splash = '/splash';
+  static const String home = '/home';
+  static const String detail = '/detail';
 }
