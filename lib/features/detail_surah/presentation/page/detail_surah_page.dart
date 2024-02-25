62c37d3ebd240b37ffe4385ded188026127ce537
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2024-02-25 07:32:21 +0700

    Add detail header, opening bismillah, and verses to detail surah page

--- lib/features/detail_surah/presentation/page/detail_surah_page.dart
+++ lib/features/detail_surah/presentation/page/detail_surah_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+import '../../../../core/core.dart';
 import '../../../../injection.dart';
 import '../../domain/domain.dart';
 import '../presentation.dart';
+
+part 'detail_surah_page.component.dart';
 
 class DetailSurahPage extends StatefulWidget {
   const DetailSurahPage({super.key, required this.surahNumber});
@@ -30,26 +36,50 @@ class _DetailSurahPageState extends State<DetailSurahPage> {
       create: (context) => detailSurahBloc,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Surah Detail'),
+          titleSpacing: 0.0,
+          title: BlocBuilder<DetailSurahBloc, DetailSurahState>(
+            builder: (context, state) {
+              if (state is DetailSurahLoaded) {
+                SurahDetail surahDetail = state.detailSurah;
+                return Row(
+                  children: [
+                    Text(
+                      '${surahDetail.number}. ',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      surahDetail.name!.transliteration!.id ?? 'Error',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                );
+              } else {
+                return const Text('Detail Surah');
+              }
+            },
+          ),
         ),
         body: BlocBuilder<DetailSurahBloc, DetailSurahState>(
           builder: (context, state) {
             if (state is DetailSurahLoaded) {
               SurahDetail surahDetail = state.detailSurah;
-              return ListView(
-                children: [
-                  Text(surahDetail.number.toString()),
-                  Text(surahDetail.name!.translation!.en ?? ''),
-                  Text(surahDetail.name!.transliteration!.en ?? ''),
-                ],
+              List<Verse> verses = surahDetail.verses!;
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    DetailHeader(surahDetail: surahDetail),
+                    OpeningBismillah(surahDetail: surahDetail),
+                    Verses(verses: verses),
+                  ],
+                ),
               );
             } else if (state is DetailSurahLoading) {
               return const Center(
                 child: CircularProgressIndicator(),
-              );
-            } else if (state is DetailSurahError) {
-              return const Center(
-                child: Text('Error'),
               );
             } else {
               return const Center(
