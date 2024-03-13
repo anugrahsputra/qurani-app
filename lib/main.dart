96ef315f5ae9688b2bf68bdd41003c6dfed723e6
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2024-03-13 08:22:18 +0700

    Update color scheme in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Qurani',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
         useMaterial3: true,
         fontFamily: GoogleFonts.notoSans().fontFamily,
       ),
