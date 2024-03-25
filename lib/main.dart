5f9b2f1a4098f6de289758a86e7aa8ebcb029696
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2024-03-25 23:02:34 +0700

    Add Hive database initialization and bookmark functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:qurani/core/core.dart';
 
+import 'features/bookmark/bookmark.dart';
 import 'injection.dart';
+import 'observer.dart';
 
 Future<void> main() async {
   CustomLog.initialize(showLog: true);
+  Bloc.observer = MyBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
   await setup();
+  await Hive.initFlutter();
+  Hive.registerAdapter(BookmarkAdapter());
+  await Hive.openBox<BookmarkModel>('bookmarks');
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final BookmarkBloc bookmarkBloc = sl<BookmarkBloc>();
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Qurani',
-      theme: AppThemes.light,
-      initialRoute: AppRoutes.initial,
-      routes: AppRoutes.routes,
+    return BlocProvider(
+      create: (context) => bookmarkBloc..add(const BookmarkGet()),
+      child: MaterialApp(
+        title: 'Qurani',
+        theme: AppThemes.light,
+        initialRoute: AppRoutes.initial,
+        routes: AppRoutes.routes,
+      ),
     );
   }
 }
