ab477bbffe2d7f2ff27572f238fa0b73097b2328
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2024-04-24 05:03:28 +0700

    Refactor AyahPage widget to include screenshot functionality and share button in ayah_page.dart

--- lib/presentation/pages/ayah/ayah_page.component.dart
+++ lib/presentation/pages/ayah/ayah_page.component.dart
@@ -1,11 +1,10 @@
 part of 'ayah_page.dart';
 
-class AyahView extends StatelessWidget {
-  const AyahView({
+class AyahImagePreview extends StatelessWidget {
+  const AyahImagePreview({
     super.key,
     required this.ayah,
   });
-
   final Ayah ayah;
 
   @override
@@ -13,72 +12,211 @@ class AyahView extends StatelessWidget {
     String ayahSource =
         '${ayah.surah.name.transliteration.id} ${ayah.surah.number}: ${ayah.number.inSurah}';
 
+    var size = MediaQuery.sizeOf(context);
     return Container(
-      margin: const EdgeInsets.all(20),
-      padding: const EdgeInsets.all(15),
+      height: size.height,
+      width: size.width,
+      padding: EdgeInsets.symmetric(
+        vertical: 30.h,
+        horizontal: 20.w,
+      ),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: AppColors.shadow.withOpacity(0.85),
-            offset: const Offset(3, 4),
-            blurRadius: 14,
-            spreadRadius: -9,
-          )
-        ],
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            AppColors.primaryContainer,
+            AppColors.primary,
+          ],
+        ),
       ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Text(
-              ayahSource,
-              style: TextStyle(
-                fontSize: 20.sp,
-                fontWeight: FontWeight.w600,
-                color: Colors.black,
+      child: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Container(
+              height: constraints.maxHeight * 0.5,
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.r),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: AppColors.shadow.withOpacity(0.85),
+                    offset: const Offset(4, 4),
+                    blurRadius: 14,
+                    spreadRadius: -8,
+                  )
+                ],
               ),
-            ),
-          ),
-          Gap(30.h),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Text(
-              ayah.text.arab,
-              style: GoogleFonts.amiri(
-                height: 2,
-                fontWeight: FontWeight.bold,
-                fontSize: 24.sp,
-                color: Colors.black,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      ayahSource,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  Gap(30.h),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      ayah.text.arab,
+                      style: GoogleFonts.amiri(
+                        height: 2,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24.sp,
+                        color: Colors.black,
+                      ),
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                  Gap(15.h),
+                  Text(
+                    ayah.translation.id,
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                  const Spacer(),
+                  Text(
+                    'Qurani v1.0',
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
               ),
-              textDirection: TextDirection.ltr,
-              textAlign: TextAlign.end,
-            ),
-          ),
-          Gap(15.h),
-          Text(
-            ayah.text.transliteration.en,
-            style: TextStyle(
-              fontSize: 16.sp,
-              fontWeight: FontWeight.w600,
-              color: Colors.black,
-            ),
-            textAlign: TextAlign.justify,
-          ),
-          Text(
-            ayah.translation.id,
-            style: TextStyle(
-              fontSize: 16.sp,
-              color: Colors.black,
-            ),
-            textAlign: TextAlign.justify,
-          ),
-        ],
+            );
+          },
+        ),
       ),
+    );
+  }
+}
+
+class AyahView extends StatelessWidget {
+  const AyahView({
+    super.key,
+    required this.ayah,
+    this.onPressed,
+  });
+
+  final Ayah ayah;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    String ayahSource =
+        '${ayah.surah.name.transliteration.id} ${ayah.surah.number}: ${ayah.number.inSurah}';
+
+    return OpenContainer(
+      transitionDuration: const Duration(milliseconds: 500),
+      closedColor: AppColors.background,
+      openColor: AppColors.primaryContainer,
+      openElevation: 0,
+      closedElevation: 0,
+      openBuilder: (context, action) {
+        return GestureDetector(
+          onTap: () => action(),
+          child: AyahImagePreview(ayah: ayah),
+        );
+      },
+      closedBuilder: (context, action) {
+        return Container(
+          margin: const EdgeInsets.all(20),
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: AppColors.shadow.withOpacity(0.85),
+                offset: const Offset(3, 4),
+                blurRadius: 14,
+                spreadRadius: -9,
+              )
+            ],
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                top: -15,
+                right: -10,
+                child: IconButton(
+                  onPressed: onPressed,
+                  icon: const Icon(Icons.fullscreen),
+                ),
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      ayahSource,
+                      style: TextStyle(
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Gap(30.h),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      ayah.text.arab,
+                      style: GoogleFonts.amiri(
+                        height: 2,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24.sp,
+                        color: Colors.black,
+                      ),
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                  Gap(15.h),
+                  Text(
+                    ayah.text.transliteration.en,
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                  Text(
+                    ayah.translation.id,
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
@@ -87,41 +225,41 @@ class TafsirView extends StatelessWidget {
   const TafsirView({
     super.key,
     required this.ayah,
+    required this.appNavigator,
   });
 
   final Ayah ayah;
+  final AppNavigator appNavigator;
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.sizeOf(context).width,
       margin: const EdgeInsets.all(20),
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: AppColors.shadow.withOpacity(0.85),
-            offset: const Offset(3, 4),
-            blurRadius: 14,
-            spreadRadius: -9,
-          )
-        ],
-      ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Text(
-              'Tafsir ayat ke ${ayah.number.inSurah}',
-              style: TextStyle(
-                fontSize: 20.sp,
-                fontWeight: FontWeight.w600,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  'Tafsir surah ${ayah.surah.name.transliteration.id} ayat ke ${ayah.number.inSurah}',
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
               ),
-            ),
+              IconButton(
+                onPressed: () {
+                  appNavigator.back(context);
+                },
+                icon: const Icon(Icons.close_rounded),
+              )
+            ],
           ),
           Gap(15.h),
           Container(
--- lib/presentation/pages/ayah/ayah_page.dart
+++ lib/presentation/pages/ayah/ayah_page.dart
@@ -1,9 +1,17 @@
+import 'dart:developer';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:screenshot/screenshot.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../../../core/core.dart';
 import '../../../features/ayah/ayah.dart';
@@ -28,8 +36,9 @@ class AyahPage extends StatefulWidget {
 
 class _AyahPageState extends State<AyahPage> {
   final AyahsBloc ayahsBloc = sl<AyahsBloc>();
+  final AppNavigator appNavigator = sl<AppNavigator>();
 
-  final ScreenshotController _screenshotController = ScreenshotController();
+  late Uint8List image;
 
   int get surahNumber => widget.surahNumber;
   int get ayahNumber => widget.ayahNumber;
@@ -48,54 +57,8 @@ class _AyahPageState extends State<AyahPage> {
         appBar: AppBar(
           title: const Text('Ayah'),
           backgroundColor: Colors.transparent,
-          actions: [
-            BlocBuilder<AyahsBloc, AyahsState>(
-              builder: (context, state) {
-                final result = state is AyahLoaded;
-                if ((state is AyahInitial) || (state is AyahLoading)) {
-                  return IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.share_rounded),
-                  );
-                } else if (result) {
-                  return IconButton(
-                    onPressed: () {
-                      AppSnackbar.showSnackBar(context,
-                          message: 'Getting image...',
-                          snackbarColor: AppColors.onBackground);
-                      _screenshotController
-                          .captureFromWidget(AyahView(ayah: state.ayah),
-                              delay: const Duration(milliseconds: 800))
-                          .then(
-                        (imgCaptured) {
-                          // TODO: show dialog to preview image before sharing them
-                          showAdaptiveDialog(
-                            context: context,
-                            builder: (context) {
-                              return Container(
-                                width: MediaQuery.sizeOf(context).width * 0.5,
-                                height: MediaQuery.sizeOf(context).height * 0.5,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20.r),
-                                  color: AppColors.onPrimary,
-                                ),
-                                child: Image.memory(imgCaptured),
-                              );
-                            },
-                          );
-                        },
-                      );
-                    },
-                    icon: const Icon(Icons.share_rounded),
-                  );
-                } else {
-                  return IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.share_rounded),
-                  );
-                }
-              },
-            ),
+          actions: const [
+            ShareIcon(),
           ],
         ),
         body: BlocBuilder<AyahsBloc, AyahsState>(
@@ -109,11 +72,24 @@ class _AyahPageState extends State<AyahPage> {
               return SingleChildScrollView(
                 child: Column(
                   children: [
-                    Screenshot(
-                      controller: _screenshotController,
-                      child: AyahView(ayah: ayah),
+                    AyahView(ayah: ayah),
+                    FilledButton.icon(
+                      onPressed: () {
+                        showMaterialModalBottomSheet(
+                          clipBehavior: Clip.antiAlias,
+                          context: context,
+                          builder: (context) => SingleChildScrollView(
+                            controller: ModalScrollController.of(context),
+                            child: TafsirView(
+                              ayah: ayah,
+                              appNavigator: appNavigator,
+                            ),
+                          ),
+                        );
+                      },
+                      icon: const Icon(Icons.book),
+                      label: const Text('Baca Tafsir'),
                     ),
-                    TafsirView(ayah: ayah),
                   ],
                 ),
               );
@@ -123,6 +99,85 @@ class _AyahPageState extends State<AyahPage> {
           },
         ),
       ),
+    );
+  }
+}
+
+class ShareIcon extends StatefulWidget {
+  const ShareIcon({super.key});
+
+  @override
+  State<ShareIcon> createState() => _ShareIconState();
+}
+
+class _ShareIconState extends State<ShareIcon> {
+  final ScreenshotController _screenshotController = ScreenshotController();
+
+  late Uint8List image;
+
+  void shareButton(Ayah ayah) async {
+    AppSnackbar.showSnackBar(context,
+        message: 'Getting image...', snackbarColor: AppColors.onBackground);
+
+    final mediaQuery = MediaQuery.of(context);
+    final pixelRatio = mediaQuery.devicePixelRatio;
+    _screenshotController
+        .captureFromWidget(
+      MediaQuery(
+          data: mediaQuery.copyWith(devicePixelRatio: pixelRatio * 2),
+          child: AyahImagePreview(ayah: ayah)),
+      delay: const Duration(milliseconds: 1000),
+    )
+        .then(
+      (imgCaptured) async {
+        image = imgCaptured;
+        String fileName =
+            "${ayah.surah.name.transliteration.id}_${ayah.surah.number}${ayah.number.inSurah}"
+                .replaceAll('-', '_')
+                .toLowerCase();
+
+        final dir = await getApplicationDocumentsDirectory();
+        final imagePath = await File('${dir.path}/$fileName.png').create();
+        await imagePath.writeAsBytes(image);
+
+        log('image path $imagePath');
+        await Share.shareXFiles(
+          [XFile(imagePath.path)],
+          text: ayah.surah.name.transliteration.id,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AyahsBloc, AyahsState>(
+      builder: (context, state) {
+        if ((state is AyahInitial) || (state is AyahLoading)) {
+          return IconButton(
+            onPressed: () => AppSnackbar.showSnackBar(
+              context,
+              message: 'Page still loading',
+              snackbarColor: AppColors.errorContainer,
+            ),
+            icon: const Icon(Icons.share_rounded),
+          );
+        } else if (state is AyahLoaded) {
+          return IconButton(
+            onPressed: () => shareButton(state.ayah),
+            icon: const Icon(Icons.share_rounded),
+          );
+        } else {
+          return IconButton(
+            onPressed: () => AppSnackbar.showSnackBar(
+              context,
+              message: 'Page still loading',
+              snackbarColor: AppColors.errorContainer,
+            ),
+            icon: const Icon(Icons.share_rounded),
+          );
+        }
+      },
     );
   }
 }
