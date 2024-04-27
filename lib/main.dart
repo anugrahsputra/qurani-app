eb2931e89e7f68745da0647983b78024c57cfb02
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2024-04-28 05:57:06 +0700

    Refactor app.dart and injection.dart, and update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,7 @@
-import 'package:device_preview_screenshot/device_preview_screenshot.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:qurani/core/core.dart';
-import 'package:screenshot/screenshot.dart';
-
-import 'injection.dart';
-import 'observer.dart';
-import 'presentation/presentation.dart';
+import 'package:qurani/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  CustomLog.initialize(showLog: kDebugMode);
-  Bloc.observer = MyBlocObserver();
-  await setup();
-  ScreenshotController screenshotController = ScreenshotController();
-
-  runApp(DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) =>
-        Screenshot(controller: screenshotController, child: const MyApp()),
-    tools: const [
-      ...DevicePreview.defaultTools,
-      DevicePreviewScreenshot(),
-    ],
-  ));
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => sl<AppbarBloc>(),
-        ),
-      ],
-      child: ScreenUtilInit(
-        designSize: const Size(412.0, 916.0),
-        builder: (context, child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            locale: DevicePreview.locale(context),
-            builder: DevicePreview.appBuilder,
-            title: 'Qurani',
-            theme: AppThemes.light,
-            initialRoute: AppRoutes.initial,
-            routes: AppRoutes.routes,
-          );
-        },
-      ),
-    );
-  }
+  await initialize();
 }
