86ad98783561f8a20ed88bf91945c8c48e100190
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2024-05-07 19:22:39 +0700

    feat: Refactor SearchPage components

--- lib/presentation/pages/search/search_page.component.dart
+++ lib/presentation/pages/search/search_page.component.dart
@@ -3,21 +3,25 @@ part of 'search_page.dart';
 class SearchTextField extends StatelessWidget {
   const SearchTextField({
     super.key,
+    required this.focusNode,
     required this.surahBloc,
   });
 
   final SurahBloc surahBloc;
-
+  final FocusNode focusNode;
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10.w),
+      margin: EdgeInsets.only(right: 10.w),
+      height: 50.h,
       child: TextField(
+        focusNode: focusNode,
         onChanged: (query) {
           surahBloc.add(OnSearchSurah(query));
         },
         decoration: InputDecoration(
           hintText: "cth: 'Al-Baqarah'",
+          contentPadding: EdgeInsets.zero,
           prefixIcon: const Icon(Icons.search),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(16.r),
@@ -43,51 +47,47 @@ class SearchList extends StatelessWidget {
           return const Center(child: CircularProgressIndicator());
         } else if (state is SurahLoaded) {
           List<SurahEntity> surahs = state.listSurah;
-          return Expanded(
-            child: ListView.builder(
-              itemCount: surahs.length,
-              itemBuilder: (context, index) {
-                var surah = surahs[index];
-                return InkWell(
-                  child: SurahCardView(surah: surah),
-                  onTap: () => appNavigator.goToDetail(
-                    context,
-                    surahNumber: surah.number,
-                  ),
-                );
-              },
-            ),
-          );
+          return BuildSurahList(listSurah: surahs);
         } else if (state is SurahSearched) {
           List<SurahEntity> filteredSurahs = state.filteredSurah;
-
-          if (filteredSurahs.isEmpty) {
-            return const Align(
-              alignment: Alignment.center,
-              child: Text('not found'),
-            );
-          }
-          return Expanded(
-            child: ListView.builder(
-              itemCount: filteredSurahs.length,
-              itemBuilder: (context, index) {
-                var surah = filteredSurahs[index];
-                return InkWell(
-                  child: SurahCardView(surah: surah),
-                  onTap: () => appNavigator.goToDetail(
-                    context,
-                    surahNumber: surah.number,
-                  ),
-                );
-              },
-            ),
-          );
+          return filteredSurahs.isEmpty
+              ? const Align(
+                  alignment: Alignment.center,
+                  child: Text('not found'),
+                )
+              : BuildSurahList(listSurah: filteredSurahs);
         } else {
           return const Align(
             alignment: Alignment.center,
             child: Text('idk wtf is going on here'),
           );
         }
+      },
+    );
+  }
+}
+
+class BuildSurahList extends StatelessWidget {
+  const BuildSurahList({
+    super.key,
+    required this.listSurah,
+  });
+
+  final List<SurahEntity> listSurah;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: listSurah.length,
+      itemBuilder: (context, index) {
+        var surah = listSurah[index];
+        return InkWell(
+          onTap: () => sl<AppNavigator>().goToDetail(
+            context,
+            surahNumber: surah.number,
+          ),
+          child: SurahCardView(surah: surah),
+        );
       },
     );
   }
