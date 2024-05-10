0b405299288157a076b3f0bddcbbdd9ad067383d
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2024-05-11 01:15:35 +0700

    feat: Add Flutter Native Splash package for custom launch screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:qurani/app.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await initialize();
+
+  FlutterNativeSplash.remove();
 }
