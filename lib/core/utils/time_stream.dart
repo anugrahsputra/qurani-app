62b6774eb7e34d9f0d112e980ee773dfe338a19c
anugrahsputra <anugrahsputra@gmail.com>
2025-07-29 20:50:57 +0700

    refactor: Ensure Timer is cancelled in TimeStream
    This commit updates the `TimeStream` class to properly cancel its internal `Timer` when the stream is disposed.
    
    - A `Timer? _timer` field is added to hold the `Timer.periodic` instance.
    - The `Timer` is now cancelled in the `dispose` method.
    - A check `!_controller.isClosed` is added before adding to the sink to prevent errors after disposal.

--- lib/core/utils/time_stream.dart
+++ lib/core/utils/time_stream.dart
@@ -2,16 +2,20 @@ import 'dart:async';
 
 class TimeStream {
   final _controller = StreamController<DateTime>.broadcast();
+  Timer? _timer;
 
   Stream<DateTime> get stream => _controller.stream;
 
   TimeStream() {
-    Timer.periodic(const Duration(seconds: 1), (timer) {
-      _controller.sink.add(DateTime.now());
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (!_controller.isClosed) {
+        _controller.sink.add(DateTime.now());
+      }
     });
   }
 
   void dispose() {
+    _timer?.cancel();
     _controller.close();
   }
 }
