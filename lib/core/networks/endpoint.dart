5c74f382e4fd198a37859ee35bfe47189443bc7c
anugrahsputra <anugrahsputra@gmail.com>
2025-08-09 03:14:07 +0700

    add debug url

--- lib/core/networks/endpoint.dart
+++ lib/core/networks/endpoint.dart
@@ -1,10 +1,19 @@
+import 'dart:io';
+
 abstract class Endpoint {
   static String get _quranBaseUrl => "https://api.quran.gading.dev/";
-  static String get _quranBaseUrlDebug => "http://10.0.2.2:3000/";
+  static String get _quranBaseUrlAndroidDebug =>
+      "http://10.0.2.2:3000/"; // android
+  static String get _quranBaseUrlIosDebug => "http://10.0.2.2:3000/"; // android
   static String get _audioBaseUrl => "https://api.quran.com/api/v4/";
+
+  static bool get isAndroid => Platform.isAndroid;
+
+  static String get _quranBaseUrlDebug =>
+      isAndroid ? _quranBaseUrlAndroidDebug : _quranBaseUrlIosDebug;
 
   static String get recitalSurah => "${_audioBaseUrl}chapter_recitations/7/";
 
-  static String get surah => "${_quranBaseUrlDebug}surah";
+  static String get surah => "${_quranBaseUrl}surah";
   static String get ayah => "${_quranBaseUrl}surah";
 }
